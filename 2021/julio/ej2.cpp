752211f5651f3693bf70a94daa4cf1e09bc55ac8
Yliana <ylianaotero@gmail.com>
2023-02-11 16:10:04 -0300

    Parte b)

--- 2021/julio/ej2.cpp
+++ 2021/julio/ej2.cpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /*Considere el TAD Cola no acotada de enteros (ColaInt), que contempla la política FIFO, con operaciones 
 exclusivamente para: crear una cola vacía; insertar; eliminar y retornar el elemento correspondiente; y chequear 
 si una cola está vacía.
@@ -15,5 +17,45 @@ bool esVacia(ColaInt c);
 void encolar(int elem, ColaInt cola);
 
 //PRE: !esVacia(cola);
-//POS: Elimina el primer elemento de la cola y lo devuelve.
-int desencolar(ColaInt cola);
+//POS: Elimina el primer elemento de la cola.
+void desencolar(ColaInt cola);
+
+//PRE: !esVacia(cola);
+//POS: Devuelve el primer elemento de la cola.
+int frente(ColaInt cola);
+
+
+/*b)Implemente la función: ColaInt camino (ABB t, int x), que dado un árbol binario de búsqueda (ABB) de 
+enteros ‘t’ y un entero ‘x’, retorne una cola de tipo ColaInt que contenga el camino a ‘x’ desde la raíz de ‘t’. 
+En la cola resultado el primero debe corresponder a la raíz de ‘t’ y el último elemento, según la política FIFO, 
+debe ser ‘x’. Si ‘x’ no está en ‘t’, el resultado deberá ser la cola vacía. La operación camino no puede acceder 
+a la representación del TAD ni modificar sus parámetros. Considere:*/
+typedef struct nodoABB * ABB;
+struct nodoABB { int dato; ABB izq, der; };
+
+void caminoAux (ABB t, int x, ColaInt &cola) {
+    if (t != NULL) {
+        encolar(t->dato, cola);
+
+        if (t->dato > x) {
+            caminoAux(t->izq, x, cola);
+        } else if (t->dato < x) {
+            caminoAux(t->der, x, cola);
+        }
+
+    } else {
+        while (!esVacia(cola)) {
+            desencolar(cola);
+        }
+    }
+}
+
+ColaInt camino (ABB t, int x) {
+    ColaInt cola = crearCola();
+
+    if (t != NULL) {
+        caminoAux(t, x, cola);
+    }
+
+    return cola;
+}
