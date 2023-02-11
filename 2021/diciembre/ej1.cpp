c67d0b05a4c36d449023bd17f9c1c5a8cdca7cf3
Yliana <ylianaotero@gmail.com>
2023-02-10 21:20:54 -0300

    Correcciones moverPrimero

--- 2021/diciembre/ej1.cpp
+++ 2021/diciembre/ej1.cpp
@@ -15,38 +15,33 @@ struct repT
 typedef struct repT *Tree;
 
 /* Rota los subárboles de cada nodo de T y devuelve el resultado de la transformación.*/
-//Tree rotar(Tree T);
+// Tree rotar(Tree T);
 
 /* (a) Implemente una función iterativa que aplica la rotación descrita solo al nodo pasado por parámetro (no
 a sus subárboles). */
 
 /*Devuelve el árbol resultado de mover el primer subárbol de 'T' dejándolo como su último subárbol.
 Pre: T != NULL.*/
-Tree moverPrimero(repT *T) {
-    Tree primero = T->ph;
-    Tree auxPrimero = primero;
-    Tree auxSegundo = T->sh;
-
-    T->ph = T->sh;
-
-    Tree auxHermano = auxPrimero->sh;
-    
-    while (auxPrimero->sh != NULL) {
-        auxHermano = auxPrimero->sh;
-        auxPrimero = auxHermano;
-        auxHermano = auxHermano->sh;
+Tree moverPrimero(repT *T)
+{
+    if ((T->ph != NULL) && (T->ph->sh != NULL)) {
+        repT * aMover = T->ph;
+        T->ph = T->ph->sh;
+        aMover->sh = NULL;
+        repT * cursor = T->ph;
+        while (cursor->sh != NULL) {
+            cursor = cursor->sh;
+        }
+        cursor->sh = aMover;
     }
-
-    Tree auxUltimo = auxPrimero;
-    T->ph = auxHermano;
-    auxHermano = auxUltimo;
-
 
     return T;
 };
 
-Tree rotar(Tree T) {
-    if (T != NULL) {
+Tree rotar(Tree T)
+{
+    if (T != NULL)
+    {
         T = moverPrimero(T);
         T->ph = rotar(T->ph);
         T->sh = rotar(T->sh);
@@ -55,68 +50,35 @@ Tree rotar(Tree T) {
     return T;
 }
 
-Tree crearArbol(int info)
-{
-    Tree t = new repT;
-    t->dato = info;
-    t->sh = NULL;
-    t->ph = NULL;
-
-    return t;
-};
-
-void agregarNodo(Tree t, int info, bool pH)
-{
-    Tree nuevoNodo = crearArbol(info);
-    Tree aux = t;
-
-    if (pH)
-    {
-        t->ph = nuevoNodo;
-    }
-    else
-    {
-
-      //  if (t->ph != NULL) aux = t->ph;
-
-
-        while (aux->sh != NULL)
-        {
-            aux = aux->sh;
-        }
-
-        aux->sh = nuevoNodo;
-    }
-};
 
 int main()
 {
-    Tree t = crearArbol(-1);
-    Tree aux = t;
+    Tree t = new repT;
+    t->dato = 1;
+    t->sh = NULL;
 
-    agregarNodo(aux, 19, true);
+    t->ph = new repT;
+    t->ph->dato = 2;
+    t->ph->sh = new repT;
+    t->ph->sh->dato = 3;
+    t->ph->sh->sh = new repT;
+    t->ph->sh->sh->dato = 4;
+    t->ph->sh->sh->sh = NULL;
     
-
-    for (int i = 0; i < 4; i++)
-    {
-        agregarNodo(t, i, false);
-    }
-
-    Tree rotado = moverPrimero(t);
+    Tree aux = t;
 
     printf("PRIMER NIVEL ARBOL ORIGINAL: \n");
     printf("%d", t->dato);
-    printf("\n");
-
-    printf("PRIMER NIVEL ARBOL ROTADO: \n");
-    printf("%d", rotado->dato);
     printf("\n");
 
     printf("SEGUNDO NIVEL ARBOL ORIGINAL: \n");
     printf("%d", t->ph->dato);
     printf(" ");
 
-    while(t->sh != NULL) {
+    t = t->ph;
+
+    while (t->sh != NULL)
+    {
         t = t->sh;
         printf("%d", t->dato);
         printf(" ");
@@ -124,19 +86,25 @@ int main()
 
     printf("\n");
 
+    Tree rotado = moverPrimero(aux);
+
+    printf("PRIMER NIVEL ARBOL ROTADO: \n");
+    printf("%d", rotado->dato);
+    printf("\n");
+
+
     printf("SEGUNDO NIVEL ARBOL ROTADO: \n");
     printf("%d", rotado->ph->dato);
     printf(" ");
 
-    while(rotado->sh != NULL) {
+    rotado = rotado->ph;
+
+    while (rotado->sh != NULL)
+    {
         rotado = rotado->sh;
         printf("%d", rotado->dato);
         printf(" ");
     }
 
     printf("\n");
-
-
-
- 
 }
