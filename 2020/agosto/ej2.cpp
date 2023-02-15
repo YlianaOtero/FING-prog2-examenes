8c799aa0e3500cee9d2f1e3739cc3c07fd3a4565
YlianaOtero <ylianaotero@gmail.com>
2023-02-15 16:11:39 -0300

    Create ej2.cpp

---
+++ 2020/agosto/ej2.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/*Considere la siguiente especificación del TAD Tabla no acotada de unsigned int (dominio) en float (codominio):*/
+struct RepTabla;
+typedef RepTabla * Tabla;
+typedef unsigned int nat;
+
+// POS: Devuelve la Tabla vacía, sin correspondencias.
+Tabla crear();
+
+// POS: Agrega la correspondencia (d,c) en t, si d no tenía imagen en t. En caso contrario actualiza la imagen de d con c.
+void insertar (nat d, float c, Tabla & t);
+
+// POS: Devuelve true si y sólo si d tiene imagen en t. 
+bool definida (nat d, Tabla t);
+
+// POS: Devuelve la cantidad de correspondencias en t. En particular, 0 si t es la tabla vacía.
+int cantidad (Tabla t);
+
+// PRE: definida(d,t). POS: Retorna la imagen de d en t.
+float recuperar (nat d, Tabla t);
+
+// POS: Elimina de t la correspondencia que involucra a d, si d está definida en t. En otro caso la operación no tieneefecto.
+void eliminar (nat d, Tabla & t);
+
+/*Implemente el TAD Tabla de tal manera que cantidad tenga O(1) en el peor caso e insertar, recuperar y eliminar
+tengan O(log2(n)) en el caso promedio, siendo n la cantidad de correspondencias de la tabla. Desarrolle la
+representación del TAD (RepTabla) y el código de las operaciones crear, cantidad e insertar. Omita el código del
+resto de las operaciones del TAD, que puede asumir implementadas. No use TADs auxiliares en la
+implementación; utilice estructuras de datos.*/
+
+struct Nodo {
+    nat d;
+    float c;
+    Nodo * izq;
+    Nodo * der;
+};
+
+typedef Nodo * ABB;
+
+struct RepTabla {
+    ABB abb;
+    int cant;
+};
+
+typedef RepTabla * Tabla;
+
+Tabla crear() {
+    Tabla t = new RepTabla;
+    t->abb = NULL;
+    t->cant = 0;
+};
+
+void insertar (nat d, float c, Tabla & t) {
+    if (t)
+};
